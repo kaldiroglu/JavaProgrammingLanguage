36b0100eac60c3423f698d1fad9c45c140b08777
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2025-11-16 14:17:26 +0300

    Source files added.

--- src/org/javaturk/jp/ch02/ShortestHelloWorld.java
+++
@@ -1,6 +0,0 @@
-
-
-
-void main() {
-    System.out.println("Hello World!");
-}
--- src/org/javaturk/jp/ch04/ValuesOfExpressions.java
+++ src/org/javaturk/jp/ch04/ValuesOfExpressions.java
@@ -1,13 +1,5 @@
 package org.javaturk.jp.ch04;
 
-/**
- * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
- *         <p>
- *         Daha cok bilgi icin lutfen aşağıdaki linkleri ziyaret edin.
- * @see <a href="http://www.javaturk.org">http://www.javaturk.org</a>
- * @see <a href="http://www.selsoft.com.tr">http://www.selsoft.com.tr</a>
- *      </p>
- */
 public class ValuesOfExpressions {
 
 	public static void main(String[] args) {
