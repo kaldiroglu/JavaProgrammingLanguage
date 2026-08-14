683138a534e3fc7f0d200ac73533e63d6bddb9fb
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-08-14 15:25:13 +0300

    Some small changes.

--- src/org/javaturk/jp/ch07/car/CarTest.java
+++ src/org/javaturk/jp/ch07/car/CarTest.java
@@ -1,13 +1,5 @@
 package org.javaturk.jp.ch07.car;
 
-/**
- * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
- *         <p>
- *         Daha cok bilgi icin lutfen aşağıdaki linkleri ziyaret edin.
- * @see <a href="http://www.javaturk.org">http://www.javaturk.org</a>
- * @see <a href="http://www.selsoft.com.tr">http://www.selsoft.com.tr</a>
- *      </p>  
- */
 public class CarTest {
 
 	public static void main(String[] args) {
