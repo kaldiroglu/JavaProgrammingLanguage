6689ebee7862258221882a41b66865ca6f0f55b7
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-08-14 18:19:17 +0300

    Some small changes.

--- src/org/javaturk/jp/hw/ch08/FootballPlayerTest.java
+++ src/org/javaturk/jp/hw/ch08/FootballPlayerTest.java
@@ -1,6 +1,6 @@
 package org.javaturk.jp.hw.ch08;
 
-public class FootballPlayerTest {
+class FootballPlayerTest {
     /*
     public static void main(String[] args) {
         FootballPlayer alex = new FootballPlayer();
