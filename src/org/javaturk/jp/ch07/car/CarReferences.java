d0c94ab85037b367483578344982650dfb128892
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-08-19 12:52:01 +0300

    Some small changes.

--- src/org/javaturk/jp/ch07/car/CarReferences.java
+++ src/org/javaturk/jp/ch07/car/CarReferences.java
@@ -1,18 +1,9 @@
 
 package org.javaturk.jp.ch07.car;
 
-/**
- * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
- *         <p>
- *         Daha cok bilgi icin lutfen aşağıdaki linkleri ziyaret edin.
- * @see <a href="http://www.javaturk.org">http://www.javaturk.org</a>
- * @see <a href="http://www.selsoft.com.tr">http://www.selsoft.com.tr</a>
- *      </p>  
- */
-public class CarReferences {
+class CarReferences {
 
 	public static void main(String[] args) {
-
 		Car car1 = new Car();
 		car1.make = "Mercedes";
 		car1.model = "C200";
