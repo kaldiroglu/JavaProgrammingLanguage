2792f8f6894a628c71dab882a190d3c94da8d74e
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-12 00:17:09 +0300

    Some small changes.

---
+++ src/org/javaturk/jp/hw/ch22/StringOperations.java
@@ -0,0 +1,20 @@
+package org.javaturk.jp.hw.ch22;
+
+//import org.apache.commons.lang3.StringUtils;
+import java.io.UnsupportedEncodingException;
+
+public class StringOperations {
+//    public static void main(String[] args) throws UnsupportedEncodingException {
+//        System.out.println("Hello World!".toUpperCase());
+//        System.out.println(StringUtils.capitalize("hello world"));
+//        System.out.println(StringUtils.reverse("Hello World!"));
+//        System.out.println(StringUtils.reverse("Hello World!").toUpperCase());
+//        System.out.println(StringUtils.reverse("Hello World!   ").trim());
+//        System.out.println(StringUtils.reverse("Hello World!").substring(0, 5));
+//
+//        System.out.println(StringUtils.difference("Hello World!", "Hello Java!"));
+//        byte[] bytes = StringUtils.getBytes("Hello World!", "UTF-8");
+//        for(byte b : bytes)
+//            System.out.print((char) b + " ");
+//    }
+}
