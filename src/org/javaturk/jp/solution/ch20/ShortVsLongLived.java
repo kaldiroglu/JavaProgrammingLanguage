86deb7c34790d9946940ed558872957e8d12f5af
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-11 15:37:43 +0300

    Some small changes.

--- src/org/javaturk/jp/solution/ch20/ShortVsLongLived.java
+++ src/org/javaturk/jp/solution/ch20/ShortVsLongLived.java
@@ -33,7 +33,7 @@ class ShortVsLongLived {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         MemoryProbe.show("at the start");
         shortLived(300);
         MemoryProbe.show("after 300 short lived blocks");
