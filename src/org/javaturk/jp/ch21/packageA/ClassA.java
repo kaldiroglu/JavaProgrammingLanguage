1b240098f55ce6a91c7cdde16a74635bca1a4548
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-11 10:46:06 +0300

    Some small changes.

--- src/org/javaturk/jp/ch21/packageA/ClassA.java
+++ src/org/javaturk/jp/ch21/packageA/ClassA.java
@@ -2,18 +2,18 @@ package org.javaturk.jp.ch21.packageA;
 
 class ClassA {
 
-//	ClassX x1; // Error!
-	org.javaturk.jp.ch21.packageX.ClassX x;
-	
-	public ClassA(org.javaturk.jp.ch21.packageX.ClassX x) {
-		this.x = x;
-	}
+    //	ClassX x1; // Error!
+    org.javaturk.jp.ch21.packageX.ClassX x;
+
+    ClassA(org.javaturk.jp.ch21.packageX.ClassX x) {
+        this.x = x;
+    }
 }
 
 class ClassB {
-	org.javaturk.jp.ch21.packageX.ClassX x;
+    org.javaturk.jp.ch21.packageX.ClassX x;
 
-	void setX(org.javaturk.jp.ch21.packageX.ClassX x){
-		this.x = x;
-	}
+    void setX(org.javaturk.jp.ch21.packageX.ClassX x) {
+        this.x = x;
+    }
 }
