4b988f3a4e3807be0f3376befeb1fec42763e4ad
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-14 00:56:53 +0300

    Some small changes.

--- src/org/javaturk/jp/ch23/memberAccess/ClassA.java
+++ src/org/javaturk/jp/ch23/memberAccess/ClassA.java
@@ -2,14 +2,6 @@ package org.javaturk.jp.ch23.memberAccess;
 
 import org.javaturk.jp.ch23.memberAccess.p.ClassP;
 
-/**
- * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
- *         <p>
- *         For more info please
- * @see <a href="http://www.javaturk.org">http://www.javaturk.org</a>
- * @see <a href="http://www.selsoft.com.tr">http://www.selsoft.com.tr</a>
- *      </p>
- */
 public class ClassA{
 
 	public static void main(String[] args) {
--- src/org/javaturk/jp/ch23/memberAccess/p/ClassP.java
+++ src/org/javaturk/jp/ch23/memberAccess/p/ClassP.java
@@ -1,13 +1,5 @@
 package org.javaturk.jp.ch23.memberAccess.p;
 
-/**
- * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
- *         <p>
- *         For more info please
- * @see <a href="http://www.javaturk.org">http://www.javaturk.org</a>
- * @see <a href="http://www.selsoft.com.tr">http://www.selsoft.com.tr</a>
- *      </p>
- */
 public class ClassP {
 	public int x = 10;
 	int y = 20;
