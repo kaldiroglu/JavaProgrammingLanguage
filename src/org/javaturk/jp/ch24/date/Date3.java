6fa1c429cd38f2bd746a5d5eb1580678f64fdfc3
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-14 15:10:28 +0300

    Some small changes.

--- src/org/javaturk/jp/ch24/date/Date3.java
+++ src/org/javaturk/jp/ch24/date/Date3.java
@@ -3,9 +3,9 @@ package org.javaturk.jp.ch24.date;
 public class Date3 {
 	
 	// Use org.javaturk.jp.ch24.constants.DateConstants for int fields
-	private int dayOfMonth;
-	private int month;
-	private int year;
+	int dayOfMonth;
+	int month;
+	int year;
 
 	public Date3(int dayOfMonth, int month, int year) {
 		this.dayOfMonth = dayOfMonth;
