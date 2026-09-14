b7adced7d73431bc3d7250dc1982fab5a1794b0b
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-14 15:36:21 +0300

    Some small changes.

--- src/org/javaturk/jp/ch24/date/DateTest.java
+++ src/org/javaturk/jp/ch24/date/DateTest.java
@@ -12,7 +12,7 @@ public class DateTest {
 		
 		Date2 ed2 = new Date2("-8", "Ojak", "20019");
 		
-		Date1 ed3 = new Date1(DateConstants.DAY_8, DateConstants.JANUARY, DateConstants.YEAR_2019);
+		Date1 ed3 = new Date1(DateConstants.DAY_8, DateConstants.JANUARY, DateConstants.YEAR_2029);
 
 		Date4 ed4 = new Date4(DayOfMonth.DAY_10, Month.JUNE, Year.YEAR_2023);
 	
