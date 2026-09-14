efda26e2cd7c3cd738dd45b1e194589377be0e24
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-14 12:33:42 +0300

    Some small changes.

--- src/org/javaturk/jp/ch24/size/Sizes.java
+++ src/org/javaturk/jp/ch24/size/Sizes.java
@@ -4,46 +4,46 @@ import static org.javaturk.jp.ch24.size.Size.MEDIUM;
 import static org.javaturk.jp.ch24.size.Size.UNKNOWN;
 
 public class Sizes {
-	public static void main(String[] args) {
-//	initialize();
-		sizes();
-	}
+    public static void main(String[] args) {
+//		initialize();
+        sizes();
+    }
 
-	public static void initialize(){
-		Size size1 = Size.LARGE;
-	}
+    public static void initialize() {
+        Size size = Size.LARGE;
+    }
 
-	public static void sizes(){
-		Size size = Size.LARGE;
-		printInfo(size);
+    public static void sizes() {
+        Size size = Size.LARGE;
+        printInfo(size);
 
-		System.out.println();
+        System.out.println();
 
-		size = MEDIUM;
-		printInfo(size);
+        size = MEDIUM;
+        printInfo(size);
 
-		System.out.println();
+        System.out.println();
 
-		size = UNKNOWN;
-		printInfo(size);
+        size = UNKNOWN;
+        printInfo(size);
 
-		System.out.println(Size.getDescription());
+        System.out.println(Size.getDescription());
 
-		size = Size.getSizeForAbbreviation("XL");
-		System.out.println(size);
+        size = Size.getSizeForAbbreviation("XL");
+        System.out.println(size);
 
-		Size[] allSizes = Size.values();
-		for(Size s : allSizes)
-			System.out.println(s);
-	}
+        Size[] allSizes = Size.values();
+        for (Size s : allSizes)
+            System.out.println(s);
+    }
 
-	static void printInfo(Size size){
-		System.out.println("Size Information:");
-		System.out.println("Size: " + size);
-		String name = size.name();
-		System.out.println("Enum: " + size + " - " + size);
-		System.out.println("Ordinal: " + size.ordinal());
-		System.out.println("Abbreviation: " + size.getAbbreviation());
-		System.out.println("No: " + size.getNo());
-	}
+    static void printInfo(Size size) {
+        System.out.println("Size Information:");
+        System.out.println("Size: " + size);
+        String name = size.name();
+        System.out.println("Enum: " + size + " - " + size);
+        System.out.println("Ordinal: " + size.ordinal());
+        System.out.println("Abbreviation: " + size.getAbbreviation());
+        System.out.println("No: " + size.getNo());
+    }
 }
