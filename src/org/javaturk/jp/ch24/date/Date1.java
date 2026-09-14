c1cdc6673d8c840b082932c436fe3e8869b78806
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-14 13:10:08 +0300

    Some small changes.

--- src/org/javaturk/jp/ch24/date/Date1.java
+++ src/org/javaturk/jp/ch24/date/Date1.java
@@ -3,16 +3,16 @@ package org.javaturk.jp.ch24.date;
 
 public class Date1 {
 	
-	private int dayOfMonth;
-	private int month;
-	private int year;
+	int dayOfMonth;
+	int month;
+	int year;
 
 	public Date1(int dayOfMonth, int month, int year) {
 		this.dayOfMonth = dayOfMonth;
 		this.month = month;
 		this.year = year;
 		
-		if(dayOfMonth < 0 | dayOfMonth > 31)
+		if(dayOfMonth < 1 | dayOfMonth > 31)
 			System.out.println("Wrong day of month provided: " + dayOfMonth);
 	}
 }
--- src/org/javaturk/jp/ch24/date/Date2.java
+++ src/org/javaturk/jp/ch24/date/Date2.java
@@ -2,13 +2,13 @@ package org.javaturk.jp.ch24.date;
 
 public class Date2 {
 
-	private String dayOfMonth;
-	private String month;
-	private String year;
+    String dayOfMonth;
+    String month;
+    String year;
 
-	public Date2(String dayOfMonth, String month, String year) {
-		this.dayOfMonth = dayOfMonth;
-		this.month = month;
-		this.year = year;
-	}
+    public Date2(String dayOfMonth, String month, String year) {
+        this.dayOfMonth = dayOfMonth;
+        this.month = month;
+        this.year = year;
+    }
 }
