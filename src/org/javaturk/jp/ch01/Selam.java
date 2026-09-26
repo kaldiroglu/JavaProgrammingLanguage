7743eacf5bb0fe474236d9dd4c2f7e540dd6951c
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-09-26 20:46:50 +0300

    Some small changes.

--- src/org/javaturk/jp/ch01/Selam.java
+++ src/org/javaturk/jp/ch01/Selam.java
@@ -2,7 +2,6 @@ package org.javaturk.jp.ch01;
 
 public class Selam{
     public static void main(String[] args) {
-
         System.out.println("Selam, naber? :)");
     }
 }
---
+++ src/org/javaturk/jp/ch01/SelamNew.java
@@ -0,0 +1,7 @@
+package org.javaturk.jp.ch01;
+
+public class SelamNew {
+    void main(){
+        System.out.println("Selam from SelamNew :)");
+    }
+}
